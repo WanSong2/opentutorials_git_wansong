f2aabc893fcc5f9d6e17d5dbb4dd92f3e15c601e
WanSong2 <thddhks11111@gmail.com>
2017-09-28 19:28:40 +0900

    연습문제 커밋!!

--- index.py
+++ index.py
@@ -1,9 +1,45 @@
-number = 12222
-str = "I eat %d apples." % number
-print(str)
+pin = "881120-1068234"
+yyyymmdd = pin[0:6]
+num = pin[7:14]
+print(num)
 
 
-a = 1
-b = "python"
-c = [1,2,3]
-print(c[0])
+pin = "881120-1068234"
+spl = pin.split('-')[1]
+print(spl[0])
+
+temp = 0
+a = [1,3,5,4,2]
+for i in range(len(a)):
+    for j in range(len(a) - 1):
+        if a[j+1] > a[j]:
+            temp = a[j+1]
+            a[j+1] = a[j]
+            a[j] = temp
+print(a)
+
+
+a = ['Life', 'is', 'too', 'short']
+result = a[0] +' '+ a[1] +' '+ a[2] +' '+ a[3]
+print(result)
+
+
+a = (1, 2, 3)
+a = (1, 2, 3, 4)
+print(a)
+
+
+a = {'A':90, 'B':80, 'C':70}
+result = {'A':90, 'C':70}
+print(result)
+print(a['B'])
+
+
+a = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5]
+aSet = set(a)
+print(aSet)
+
+
+a = b = [1,2,3]
+a[1] = 4
+print(b)
